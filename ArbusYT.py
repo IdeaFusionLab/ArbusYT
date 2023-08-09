efcc4d18de7541e5e7366354f9ad90915c21666c
RaulDoblas <rauldofe7@gmail.com>
2023-08-09 19:44:33 +0200

    refactored code and added some fixes

--- ArbusYT.py
+++ ArbusYT.py
@@ -1,9 +1,17 @@
-from pytube import YouTube, Playlist
+from pytube import YouTube
 import discord
-from discord import app_commands, Intents, Interaction, Client
+from discord.ext import commands
+from discord import (
+    app_commands,
+    Intents,
+    Interaction,
+    Client,
+    Button,
+)
 import asyncio
 
-from config import DISCORD_BOT_TOKEN_YT, DOMAINS, FFMPEG_OPTIONS
+from utils import take_playlist_by_url, check_play_condition
+from config import DISCORD_BOT_TOKEN_YT, FFMPEG_OPTIONS
 
 
 class MusicBot(Client):
@@ -21,26 +29,35 @@ class MusicBot(Client):
             await channel.connect()
         self.channel = channel
 
-    async def play_audio_source(self, interaction: Interaction, url: str):
+    async def play_audio_source(
+        self, channel, interaction: Interaction, url: str
+    ):
         yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
         video = yt.streams.filter(only_audio=True).first()
 
-        voice = interaction.guild.voice_client
+        voice = channel.guild.voice_client
         voice.stop()
         audio_source = discord.FFmpegPCMAudio(video.url, **FFMPEG_OPTIONS)
 
         voice.play(
             audio_source,
-            after=lambda e: self.loop.create_task(self.play_next()),
+            after=lambda e: self.loop.create_task(self.play_next(interaction)),
+        )
+
+        await interaction.followup.send(
+            f"Reproduciendo esta cancion: {url}", view=SkipButton()
         )
 
     def enqueue(self, url: str):
         self.queue.append(url)
 
-    async def play_next(self):
-        if len(self.queue) > 0:
-            next_url = self.queue.pop(0)
-            await self.play_audio_source(self.channel, next_url)
+    async def play_next(self, interaction):
+        if not bot.channel.guild.voice_client.is_playing():
+            if len(self.queue) > 0:
+                next_url = self.queue.pop(0)
+                await self.play_audio_source(
+                    self.channel, interaction, next_url
+                )
 
     async def skip(self, interaction: Interaction):
         if (
@@ -54,9 +71,9 @@ class MusicBot(Client):
         bot.channel.guild.voice_client.stop()
         await interaction.response.send_message("Canción saltada.")
 
-        await self.play_next()
+        await self.play_next(interaction)
 
-    async def leave_voice_channel(self, interaction: Interaction, afk = False):
+    async def leave_voice_channel(self, interaction: Interaction, afk=False):
         if interaction.guild.voice_client is not None:
             await interaction.guild.voice_client.disconnect()
             bot.channel = None
@@ -75,7 +92,7 @@ class MusicBot(Client):
                 and not self.channel.guild.voice_client.is_playing()
                 and len(self.queue) == 0
             ):
-                await self.leave_voice_channel(interaction,True)
+                await self.leave_voice_channel(interaction, True)
                 break
 
     async def on_ready(self):
@@ -84,14 +101,6 @@ class MusicBot(Client):
 
 
 bot = MusicBot(intents=Intents.default())
-
-
-def check_yt_url(url):
-    url_in_domain = False
-    for domain in DOMAINS:
-        if domain in url:
-            url_in_domain = True
-    return url_in_domain
 
 
 # Commands
@@ -110,35 +119,50 @@ async def stop(interaction: Interaction):
 
 @bot.tree.command()
 async def play(interaction: Interaction, url: str):
-    if not check_yt_url(url):
-        await interaction.response.send_message("Url no valida.")
+    check_data = check_play_condition(interaction, url)
+    if not check_data['is_valid']:
+        await interaction.response.send_message(check_data['message'])
         return
-    if interaction.user.voice is None:
-        await interaction.response.send_message(
-            "Debes estar en un canal de voz para usar este comando."
-        )
-        return
-    else:
-        if not bot.channel:
-            await bot.join_voice_channel(interaction)
 
-        bot.enqueue(url)
-        await interaction.response.send_message(f'Agregado a la cola: {url}')
+    if not bot.channel:
+        await bot.join_voice_channel(interaction)
 
-        if "&list" in url:
-            playlist = Playlist(url)
-            # Agregamos los demas videos si es una playlist
-            for video_url in playlist.video_urls:
-                bot.enqueue(video_url)
+    bot.enqueue(url)
 
-        if not bot.channel.guild.voice_client.is_playing():
-            await bot.play_next()
-            bot.loop.create_task(bot.check_and_disconnect(interaction))
+    await interaction.response.send_message(f'Agregado a la cola: {url}')
+
+    # Check if url is a playlist
+    if "&list" in url:
+        video_urls = take_playlist_by_url(url)
+
+        for video_url in video_urls:
+            bot.enqueue(video_url)
+
+    # Reproduce video if none video is playing
+    if not bot.channel.guild.voice_client.is_playing():
+        await bot.play_next(interaction)
+        bot.loop.create_task(bot.check_and_disconnect(interaction))
 
 
 @bot.tree.command()
 async def skip(interaction: Interaction):
     await bot.skip(interaction)
+
+
+class SkipButton(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    @discord.ui.button(
+        label="Skip",
+        style=discord.ButtonStyle.success,
+        custom_id="skip_button",
+    )
+    async def skip_button_callback(
+        self, interaction: Interaction, button: Button
+    ):
+        await interaction.response.send_message(content="Skipped video")
+        await bot.skip(interaction)
 
 
 bot.run(DISCORD_BOT_TOKEN_YT)
